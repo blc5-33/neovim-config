76c8f4d07b8a1cc5c47fd5bb2d98096a27e94166
Upanshu Parekh <54222922+blc5-33@users.noreply.github.com>
2024-06-03 15:48:10 -0500

    Use PyRight instead of Pylyzer

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -21,7 +21,7 @@ local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('mason').setup({})
 require('mason-lspconfig').setup({
   -- ensure_installed = {'tsserver', 'rust_analyzer'},
-  ensure_installed = {'tsserver', 'eslint', 'clangd', 'lua_ls', 'pylyzer'},
+  ensure_installed = {'tsserver', 'eslint', 'clangd', 'lua_ls', 'pyright'},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({
